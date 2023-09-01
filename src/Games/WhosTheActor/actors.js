6c5db9bd6c0c11b7362441684af818e215085e71
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2023-08-31 21:04:50 -0700

    quotesandactors

---
+++ src/Games/WhosTheActor/actors.js
@@ -0,0 +1,27 @@
+const Actors = [
+    {
+        id: 1,
+        movies: ['12 Angry Men', 'My Darling Clementine', 'The Lady Eve', 'The Grapes of Wrath'],
+        actor: 'Henry Fonda',
+        options: ['Henry Fonda', 'Tim Holt', 'John Fielder', 'Charles Bronson']
+    }, 
+    {
+        id: 2,
+        movies: ['Star Wars: The Force Awakens', `The World's End`, 'Ready Player One', 'Paul'],
+        actor: 'Simon Pegg',
+        options: ['Martin Freeman', 'Simon Pegg', 'Mark Rylance', 'Nick Frost']
+    }, 
+    {
+        id: 3,
+        movies: ['Kung Fu Panda', `Donnie Darko`, 'Super Mario Bros.', 'Superbad'],
+        actor: 'Seth Rogen',
+        options: ['Jonah Hill', 'Simon Pegg', 'Seth Rogen', 'Jack Black']
+    },
+    {
+        id: 4,
+        movies: ['Kung Fu Panda', `Donnie Darko`, 'Super Mario Bros.', 'Superbad'],
+        actor: 'Seth Rogen',
+        options: ['Jonah Hill', 'Simon Pegg', 'Seth Rogen', 'Jack Black']
+    },    
+
+]
