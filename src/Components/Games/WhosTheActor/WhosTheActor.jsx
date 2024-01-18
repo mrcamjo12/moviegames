141599c8d7797ea08523602680c0d9b052938cf4
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2024-01-18 11:39:39 -0800

    gamesjsonupdate

--- src/Components/Games/WhosTheActor/WhosTheActor.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ActorGame = () => {
-    
-}
-
-
-export default ActorGame
--- src/Components/Games/WhosTheDirector/WhosTheDirector.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const DirectorGame = () => {
-    
-}
-
-
-export default DirectorGame
